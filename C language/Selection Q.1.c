150a1daaca5a1387d962e1533a8ae12abe6a3a9c
Divyesh73 <8363divyesh@gmail.com>
2024-07-01 11:48:46 +0530

    Add files via upload

---
+++ C language/Selection Q.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y;
+	printf("Enter the first number :");
+	scanf("%d",&x);
+	
+	printf("Enter the second number :");
+	scanf("%d",&y);
+	
+	(x<y)? printf("x is minimum"):("y is minimum");
+}
