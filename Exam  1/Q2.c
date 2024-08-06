222c1ba0ba3b34b5ac2de188b88f98e0e82d9790
Divyesh73 <8363divyesh@gmail.com>
2024-08-06 12:00:29 +0530

    Add files via upload

---
+++ Exam  1/Q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int age;
+	
+	printf("Enter your age : ");
+	scanf("%d",&age);
+	
+	if(18<=age)
+	{ 
+	    printf("you are aligible for vote");	
+	}
+	if(100<=age)
+	{
+		printf("this number is valid");
+	}
+	
+	
+	
+}
