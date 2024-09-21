deb1e39bbe714ec393d55bdcebd439d11eadb9a0
Divyesh73 <8363divyesh@gmail.com>
2024-09-21 10:09:16 +0530

    Update q1.c

--- C language final exam/q1.c
+++ C language final exam/q1.c
@@ -2,4 +2,18 @@
 #include<conio.h>
 
 void main()
-
+{
+	//WAP to calculate the factorial of N using while loop.
+	int i=1;
+	int n;
+	int x;
+	printf("Enter n :");
+	scanf("%d",&n);
+	
+	while(i<=n)
+	{
+		x=x*i;
+		i++;
+	}
+	printf(" Factorial of %d is %d",n,x);
+}
